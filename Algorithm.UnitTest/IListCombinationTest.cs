1ab01fbc8baad88a789d22b32307a80ddc4c07b5
Eric Xu <ericxu@microsoft.com>
2015-08-07 23:40:03 +0800

    Combinatorics

---
+++ Algorithm.UnitTest/IListCombinationTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Algorithm.Graph;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Diagnostics;
+using Algorithm.Combinatorics;
+using System.Collections;
+
+namespace Algorithm.UnitTest
+{
+    [TestClass]
+    public class IListCombinationTest
+    {
+        [TestMethod, TestCategory("Combinatorics")]
+        public void PermutationTest()
+        {
+            var data = Enumerable.Range(1, 7).ToList();
+            foreach (var entry in data.Permute(7))
+            {
+                foreach (var element in entry)
+                {
+                    Debug.Write(element + " ");
+                }
+                Debug.WriteLine("");
+            }
+        }
+
+        [TestMethod, TestCategory("Combinatorics")]
+        public void CombinationTest()
+        {
+            var data = Enumerable.Range(1, 7).ToList();
+            foreach (var entry in data.Combine(3))
+            {
+                foreach (var element in entry)
+                {
+                    Debug.Write(element + " ");
+                }
+                Debug.WriteLine("");
+            }
+        }
+    }
+}
---
+++ Algorithm/Combinatorics/IListCombination.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Combinatorics
+{
+    public static class IListCombination
+    {
+        public static IEnumerable<IList<T>> Combine<T>(this IList<T> list, int n)
+        {
+            IList<IList<T>> result = new List<IList<T>>();
+            result.Add(new List<T>());
+            for (int i = 0; i < list.Count; i++)
+            {
+                int resultCount = result.Count;
+                for (int j = 0; j < resultCount; j++)
+                {
+                    if (result[j].Count == n)
+                        continue;
+                    var entry = new List<T>(result[j]);
+                    entry.Add(list[i]);
+                    result.Add(entry);
+                }
+            }
+            for (int i = 0; i < result.Count; i++)
+            {
+                if (result[i].Count != n)
+                {
+                    result.RemoveAt(i);
+                    i--;
+                }
+            }
+            return result;
+        }
+
+        public static IEnumerable<IList<T>> Permute<T>(this IList<T> list, int n)
+        {
+            if (n == 0)
+                yield return new List<T>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                var cur = list[i];
+                list.RemoveAt(i);
+                var subCombinations = list.Permute(n - 1);
+                foreach (var item in subCombinations)
+                {
+                    item.Insert(0, cur);
+                    yield return item;
+                }
+                list.Insert(i, cur);
+            }
+        }
+    }
+}
