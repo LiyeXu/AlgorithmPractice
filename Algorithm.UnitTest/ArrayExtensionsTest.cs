45e70166c27b461cd23633fbf0b17b4a19891e4b
Eric Xu <ericxu@microsoft.com>
2016-02-18 20:56:50 -0800

    test

--- Algorithm.UnitTest/ArrayExtensionsTest.cs
+++ Algorithm.UnitTest/ArrayExtensionsTest.cs
@@ -11,6 +11,7 @@ namespace Algorithm.UnitTest
     public class ArrayExtensionsTest
     {
         // DefaultEndpointsProtocol=https;AccountName=credscantest;AccountKey=zPHCADQfmasyzTOP3hdPc1rAZfLI4CatAgJaDcsC1yDveIkpv+Q4bPm/Ar17GI3Rh7f0YWZ/A8Ab5l+VlNw72q==;BlobEndpoint=https://credscantest.blob.core.windows.net/;TableEndpoint=https://credscantest.table.core.windows.net/;QueueEndpoint=https://credscantest.queue.core.windows.net/;FileEndpoint=https://credscantest.file.core.windows.net/
+        // DefaultEndpointsProtocol=https;AccountName=credscantest;AccountKey=zPHCADQfmasyzTOP3hdPc1rAZfLI4CatAgJaDcsC1yDveIkpv+Q4bPm/Ar17GI3Rh7f0YWZ/A8Ab5l+VlNw72q==;BlobEndpoint=https://credscantest.blob.core.windows.net/;TableEndpoint=https://credscantest.table.core.windows.net/;QueueEndpoint=https://credscantest.queue.core.windows.net/;FileEndpoint=https://credscantest.file.core.windows.net/
 
         [TestMethod, TestCategory("Array")]
         public void IntersectionTest()
